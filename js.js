ed1b12a004a046a4ee2662646f33383bf728c46e
@Bahati308 <bahatibrianp@gmail.com>
2023-02-23 17:11:49 +0300

    linked js and html

--- js.js
+++ js.js
@@ -1,21 +1,21 @@
 // this js file is linked to index.html
 function closeMe() {
     // find the element
-    x=document.getElementById("demo");
+     x=document.getElementById("demo");
     // option 1: change the style attribute directly
-    x.style.display="none";
+     x.style.display="none";
 
     // option 2:
-    // x.className ="closed";
+    //  x.className="closed";
     
 }
 
-function openMe(params) {
+function openMe() {
     //find the element 
     x=document.getElementById("demo");
     //Option 1: change the style aattribute directly
-    x.style.display ="block";
+     x.style.display ="block";
 
     // Option 2:
-    // x.className="open";
+    //  x.className ="open";
 }
