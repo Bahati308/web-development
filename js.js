41d769aa01bc247f5d75f2a18468bab095a02de3
@Bahati308 <bahatibrianp@gmail.com>
2023-02-23 17:26:38 +0300

    modified js.js

--- js.js
+++ js.js
@@ -3,19 +3,19 @@ function closeMe() {
     // find the element
      x=document.getElementById("demo");
     // option 1: change the style attribute directly
-     x.style.display="none";
+    //  x.style.display="none";
 
     // option 2:
-    //  x.className="closed";
+     x.className="closed";
     
 }
 
 function openMe() {
     //find the element 
     x=document.getElementById("demo");
-    //Option 1: change the style aattribute directly
+    // Option 1: change the style aattribute directly
      x.style.display ="block";
 
     // Option 2:
-    //  x.className ="open";
+    x.className ="open";
 }
