8a38e2fdf9bc4dd788f4342b903d17c035704b1f
@Bahati308 <bahatibrianp@gmail.com>
2023-02-23 16:48:54 +0300

    modified css.css

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
 // this js file is linked to index.html
+function closeMe() {
+    // find the element
+    x=document.getElementById("demo");
+    // option 1: change the style attribute directly
+    x.stle.display="none";
+
+    // option 2:
+    x.className ="closed";
+    
+}
